98a03960cb1232d9493211aecc066d245f50dfe7
Marcos Parreiras <marcosalp1998@gmail.com>
2024-04-02 20:16:15 -0300

    feat: add add and remove to favorites button on movie-component

--- src/components/Movie/index.tsx
+++ src/components/Movie/index.tsx
@@ -1,14 +1,33 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { RootStoreState } from "../../store/store";
+import { addMovie, removeMovie } from "../../store/features/favorites";
+import { Movie as IMovie } from "../../api/MoviesService";
 
 interface MovieProps {
-  movie: {
-    title: string;
-    poster_path: string;
-    id: string;
-  };
+  movie: IMovie;
 }
 
 export default function Movie({ movie }: MovieProps) {
+  const dispatch = useDispatch();
+  const favorites = useSelector((state: RootStoreState) => state.favorites);
+  const favoritesIds = favorites.movies.map((m) => m.id);
+
+  function buttonGenerator(movieId: string, favoritesIds: string[]) {
+    if (favoritesIds.includes(movieId)) {
+      return (
+        <button onClick={() => dispatch(removeMovie(movie))}>
+          Remove from favorites
+        </button>
+      );
+    }
+    return (
+      <button onClick={() => dispatch(addMovie(movie))}>
+        Add to favorites
+      </button>
+    );
+  }
+
   return (
     <div>
       <div>
@@ -20,6 +39,7 @@ export default function Movie({ movie }: MovieProps) {
       <div>
         <h3>{movie.title}</h3>
         <Link to={`/movie/${movie.id}`}>Show details</Link>
+        {buttonGenerator(movie.id, favoritesIds)}
       </div>
     </div>
   );
--- src/store/features/favorites.ts
+++ src/store/features/favorites.ts
@@ -1,11 +1,11 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { Movie } from "../../api/MoviesService";
 
-export interface favoritesState {
+export interface FavoritesState {
   movies: Movie[];
 }
 
-const initialState: favoritesState = {
+const initialState: FavoritesState = {
   movies: [],
 };
 
@@ -13,14 +13,14 @@ const favoritesSlice = createSlice({
   name: "favorites",
   initialState,
   reducers: {
-    addMovie(state: favoritesState, action: PayloadAction<Movie>): void {
+    addMovie(state: FavoritesState, action: PayloadAction<Movie>): void {
       const movie = state.movies.find((m) => m.id === action.payload.id);
       if (movie) {
         return;
       }
       state.movies.push(action.payload);
     },
-    removeMovie(state: favoritesState, action: PayloadAction<Movie>): void {
+    removeMovie(state: FavoritesState, action: PayloadAction<Movie>): void {
       const index = state.movies.findIndex((m) => m.id === action.payload.id);
       if (index === -1) {
         return;
